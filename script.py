f72369119ab97584c6eea180e752ae16d2e45cc8
KevinHQi <klqzpro@gmail.com>
2021-01-13 16:47:15 -0800

    upload py

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import requests
-print(requests.__version__)
 
-r = requests.get('http://www.google.com/')
-# r.json()
-print(r.text)
+print("requests version: " + requests.__version__)
+
+ r = requests.get('http://www.google.com/')
+
+ print(r.text)
