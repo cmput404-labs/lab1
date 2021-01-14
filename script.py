840b291e99c311fae6600a220ceb4b2fdd92b9dc
KevinHQi <klqzpro@gmail.com>
2021-01-13 16:50:33 -0800

    fix layout

--- script.py
+++ script.py
@@ -2,6 +2,6 @@ import requests
 
 print("requests version: " + requests.__version__)
 
- r = requests.get('http://www.google.com/')
+r = requests.get('http://www.google.com/')
 
- print(r.text)
+print(r.text)
