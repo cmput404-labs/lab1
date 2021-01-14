207eb0324b788d74505a389f0f0e1879275c5ee0
KevinHQi <klqzpro@gmail.com>
2021-01-13 17:12:59 -0800

    modify script to print source code

--- script.py
+++ script.py
@@ -2,6 +2,6 @@ import requests
 
 print("requests version: " + requests.__version__)
 
-r = requests.get('http://www.google.com/')
+r = requests.get('https://raw.githubusercontent.com/cmput404-labs/lab1/main/script.py')
 
 print(r.text)
